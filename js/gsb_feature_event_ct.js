75574906a9f61652405f58087c13cce38c32451b
Greg Mercer (GBear) <gmercer@stanford.edu>
2015-07-06 07:52:39 -0700

    WP-5436 Removing some older code no longer needed

--- js/gsb_feature_event_ct.js
+++ js/gsb_feature_event_ct.js
@@ -8,53 +8,6 @@
         $('#edit-field-date-time-und-0-value-timeEntry-popup-1').val('');
       }
 
-      // location on-campus
-      $("input[id*='field-event-location-type-und-on-campus']").each(function() {
-        var index = Drupal.gsb_feature_event_ct.get_location_index($(this));
-        if (index != -1) {
-          $(this).data('item_index', index);
-        }
-      });
-      $("input[id*='field-event-location-type-und-on-campus']").on('change', function() {
-        var index = $(this).data('item_index');
-        Drupal.gsb_feature_event_ct.showhide_location_fields(index);
-      });
-
-      // location off-campus
-      $("input[id*='field-event-location-type-und-off-campus']").each(function() {
-        var index = Drupal.gsb_feature_event_ct.get_location_index($(this));
-        if (index != -1) {
-          $(this).data('item_index', index);
-        }
-      });
-      $("input[id*='field-event-location-type-und-off-campus']").on('change', function() {
-        var index = $(this).data('item_index');
-        Drupal.gsb_feature_event_ct.showhide_location_fields(index);
-      });
-
-      // location online
-      $("input[id*='field-event-location-type-und-online']").each(function() {
-        var index = Drupal.gsb_feature_event_ct.get_location_index($(this));
-        if (index != -1) {
-          $(this).data('item_index', index);
-        }
-      });
-      $("input[id*='field-event-location-type-und-online']").on('change', function() {
-        var index = $(this).data('item_index');
-        Drupal.gsb_feature_event_ct.showhide_location_fields(index);
-      });
-
-      for (var index = 0; ; index++) {
-        if ($('#edit-field-event-location-und-' + index + '-field-event-location-type-und-online').length == 0) {
-          break;
-        }
-        else {
-          if ($('#edit-field-event-location-und-' + index + '-field-event-location-type-und-online').is(':checked')) {
-            Drupal.gsb_feature_event_ct.showhide_location_fields(index);
-          }
-        }
-      }
-
       // speakers
       $("input[name$='[field_person_ref][und][0][target_id]']").each(function() {
         var index = Drupal.gsb_feature_event_ct.get_speaker_index($(this));
@@ -119,31 +72,6 @@
   }
 
   Drupal.gsb_feature_event_ct = Drupal.gsb_feature_event_ct || {}
-
-  // location functions
-
-  Drupal.gsb_feature_event_ct.get_location_index = function(element) {
-    var index = -1;
-    var name = $(element).attr('name');
-    var find = 'field_event_location[und][';
-    var part1 = name.indexOf(find);
-    if (part1 != -1) {
-      name = name.replace(find, '');
-    }
-    var part2 = name.indexOf(']');
-    if (part2 != -1) {
-      index = name.substring(0,part2);
-    }
-    return index;
-  }
-  Drupal.gsb_feature_event_ct.showhide_location_fields = function(index) {
-    if ($("input[id^='edit-field-event-location-und-" + index + "-field-event-location-type-und-online']").is(':checked')) {
-      $("div[id^='edit-field-event-location-und-" + index + "-field-address']").hide();
-    }
-    else {
-      $("div[id^='edit-field-event-location-und-" + index + "-field-address']").show();
-    }
-  }
 
   // speaker functions
 
